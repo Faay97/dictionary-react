3c610a7e4fe3ea34d3b68a91cdcee692dcc7c783
Faay97 <101605402+Faay97@users.noreply.github.com>
2022-06-17 20:32:31 -0400

    added Git link

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid " alt="logo" />
-        <h1 className="text-center head-name mt-2">thedevfaay</h1>
+        <h1 className="text-center head-name mt-2
+        ">thedevfaay</h1>
       </header>
       <Dictionary />
       <footer className="App-footer text-center mt-3">
-        Coded by Favour Umoru
+        Coded by Favour Umoru and open-sourced on <a href="https://github.com/Faay97/dictionary-react"> Github </a>
       </footer>
     </div>
   );
